186a7d23e076b3c782c77fa246eff9d427019e1f
Floris Kroeze <floris@webmen.nl>
2024-04-10 11:57:22 +0200

    Add compatibility with assetmapper

--- src/DependencyInjection/WeDevelopUXTableExtension.php
+++ src/DependencyInjection/WeDevelopUXTableExtension.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace WeDevelop\UXTable\DependencyInjection;
 
+use Symfony\Component\AssetMapper\AssetMapperInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use WeDevelop\UXTable\DataProvider\DataProviderInterface;
 use WeDevelop\UXTable\DataProvider\DoctrineORMProvider;
 use WeDevelop\UXTable\Security\OpenerSigner;
@@ -16,7 +18,7 @@ use WeDevelop\UXTable\Twig\Component\Table;
 use WeDevelop\UXTable\Twig\OpenerExtension;
 use WeDevelop\UXTable\Twig\SortExtension;
 
-final class WeDevelopUXTableExtension extends Extension
+final class WeDevelopUXTableExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
@@ -66,5 +68,33 @@ final class WeDevelopUXTableExtension extends Extension
             ->setAutowired(true)
             ->setAutoconfigured(true)
         ;
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        if ($this->isAssetMapperAvailable($container)) {
+            $container->prependExtensionConfig('framework', [
+                'asset_mapper' => [
+                    'paths' => [
+                        __DIR__.'/../../assets/src' => 'wedevelopnl/ux-table',
+                    ],
+                ],
+            ]);
+        }
+    }
+
+    private function isAssetMapperAvailable(ContainerBuilder $container): bool
+    {
+        if (!interface_exists(AssetMapperInterface::class)) {
+            return false;
+        }
+
+        // check that FrameworkBundle 6.3 or higher is installed
+        $bundlesMetadata = $container->getParameter('kernel.bundles_metadata');
+        if (!isset($bundlesMetadata['FrameworkBundle'])) {
+            return false;
+        }
+
+        return is_file($bundlesMetadata['FrameworkBundle']['path'].'/Resources/config/asset_mapper.php');
     }
 }
